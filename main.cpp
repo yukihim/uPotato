32bbf05bb626cbedb13a58d429385864355cb135
Yuyu <jacklorien@gmail.com>
2023-06-27 16:57:29 +0700

    some_changes

--- main.cpp
+++ main.cpp
@@ -1,5 +1,30 @@
-#include <windows.h>
+#include <iostream>
+#include <SDL2/SDL.h>
 
-int main() {
-    return 0;
+const int WIDTH = 800, HEIGHT = 600;
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    SDL_Window *window = SDL_CreateWindow("Hello SDL World", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
+
+    if(NULL == window) {
+        cout << "Could not create window: " << SDL_GetError() << endl;
+        return 1;
+    }
+
+    SDL_Event windowEvent;
+
+    while(true) {
+        if(SDL_PollEvent(&windowEvent)) {
+            if(SDL_QUIT == windowEvent.type)    break;
+        }
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return EXIT_SUCCESS;
 }
